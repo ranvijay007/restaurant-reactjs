cc07ee2381646ba17358122121b3129cc6d3a155
Ranvijay_Singh <vijaypop12@gmail.com>
2020-06-07 20:53:53 -0700

    single page app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,27 +6,37 @@ import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
 import Home from "./HomeComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
+import Contact from "./ContactComponent";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
         <Header />
         <Switch>
+          <Route exact path="/contactus" component={Contact} />} />
           <Route path="/home" component={HomePage} />
           <Route
             exact
