e3240b6e9bd63496e9c591f1ccfb15a3c21ae937
Ranvijay_Singh <vijaypop12@gmail.com>
2020-06-12 00:29:40 -0700

    redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,8 @@
+import { createStore } from "redux";
+import { Reducer, initialState } from "./reducers";
+
+export const ConfigureStore = () => {
+  const store = createStore(Reducer, initialState);
+
+  return store;
+};
